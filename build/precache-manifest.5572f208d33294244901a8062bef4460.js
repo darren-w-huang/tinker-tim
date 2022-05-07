a94fe795d4397b35e9860f1510fa47f87f593670
darren <dh475@uw.edu>
2022-05-06 21:43:40 -0700

    first commit

---
+++ build/precache-manifest.5572f208d33294244901a8062bef4460.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5365e4117f22f77481d7cc8f2b41537",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3cacde4da127a6dfcaa",
+    "url": "/static/css/main.ef0da3d3.chunk.css"
+  },
+  {
+    "revision": "c16d6e3f4dff639a83a6",
+    "url": "/static/js/2.9ebb8f47.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9ebb8f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3cacde4da127a6dfcaa",
+    "url": "/static/js/main.ee84be64.chunk.js"
+  },
+  {
+    "revision": "8e42e4e40bbbee2dca6a",
+    "url": "/static/js/runtime-main.c7bad3dc.js"
+  },
+  {
+    "revision": "f272f9ef5ddec1c0fa33758bda194000",
+    "url": "/static/media/cover.f272f9ef.png"
+  },
+  {
+    "revision": "f4cfe0a367825ba3442d884be6525dc9",
+    "url": "/static/media/draft.f4cfe0a3.png"
+  },
+  {
+    "revision": "b9b2756f4412462b7f430bc6a15f4fe1",
+    "url": "/static/media/tim_logo_draft.b9b2756f.png"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,146 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+// Copy the .env.example in the root into a .env file in this folder
+require('dotenv').config({ path: './.env' });
+
+// Ensure environment variables are set.
+checkEnv();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2020-08-27',
+  appInfo: { // For sample support and debugging, not required for production:
+    name: "stripe-samples/checkout-one-time-payments",
+    version: "0.0.1",
+    url: "https://github.com/stripe-samples/checkout-one-time-payments"
+  }
+});
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.urlencoded());
+app.use(
+  express.json({
+    // We need the raw body to verify webhook signatures.
+    // Let's compute it only when hitting the Stripe webhook endpoint.
+    verify: function (req, res, buf) {
+      if (req.originalUrl.startsWith('/webhook')) {
+        req.rawBody = buf.toString();
+      }
+    },
+  })
+);
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.get('/config', async (req, res) => {
+  const price = await stripe.prices.retrieve(process.env.PRICE);
+
+  res.send({
+    publicKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    unitAmount: price.unit_amount,
+    currency: price.currency,
+  });
+});
+
+// Fetch the Checkout Session to display the JSON result on the success page
+app.get('/checkout-session', async (req, res) => {
+  const { sessionId } = req.query;
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
+  res.send(session);
+});
+
+app.post('/create-checkout-session', async (req, res) => {
+  const domainURL = process.env.DOMAIN;
+  const { quantity } = req.body;
+  // Create new Checkout Session for the order
+  const session = await stripe.checkout.sessions.create({
+    mode: 'payment',
+    line_items: [
+      {
+        price: process.env.PRICE,
+        quantity: quantity
+      },
+    ],
+    success_url: `${domainURL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${domainURL}`,
+    shipping_address_collection: {
+      allowed_countries: ["US"]
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 400,
+            currency: 'usd',
+          },
+          display_name: 'Media mail',
+          // Delivers in exactly 1 business day
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 2,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 9,
+            },
+          }
+        }
+      },
+    ],
+    // automatic_tax: {enabled: true}
+  });
+
+  return res.redirect(303, session.url);
+});
+
+// Webhook handler for asynchronous events.
+app.post('/webhook', async (req, res) => {
+  let data;
+  let eventType;
+  // Check if webhook signing is configured.
+  if (process.env.STRIPE_WEBHOOK_SECRET) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    let signature = req.headers['stripe-signature'];
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    // Extract the object from the event.
+    data = event.data;
+    eventType = event.type;
+  } else {
+    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+    // retrieve the event data directly from the request body.
+    data = req.body.data;
+    eventType = req.body.type;
+  }
+
+  if (eventType === 'checkout.session.completed') {
+    console.log(`🔔  Payment received!`);
+  }
+
+  res.sendStatus(200);
+});
+
+app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
+
+
+function checkEnv() {
+  const price = process.env.PRICE;
+  if(price === "price_12345" || !price) {
+    console.log("You must set a Price ID in the environment variables. Please see the README.");
+    process.exit(0);
+  }
+}
