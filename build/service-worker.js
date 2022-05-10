d8f7dcfec727335f08b00c4560cc01b3a1e4b99a
darren <dh475@uw.edu>
2022-05-10 09:53:17 -0700

    animations, word wrapping, and clearer information

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5572f208d33294244901a8062bef4460.js"
+  "/precache-manifest.171de8ef33288a52fe591ab1a9cbe6bb.js"
 );
 
 self.addEventListener('message', (event) => {
