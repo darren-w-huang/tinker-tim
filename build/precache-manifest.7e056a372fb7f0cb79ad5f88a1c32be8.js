f562722bf67ab7cc4661c436e8d7dff01aa2728c
darren <dh475@uw.edu>
2022-05-17 15:39:23 -0700

    mobile friendly

---
+++ build/precache-manifest.7e056a372fb7f0cb79ad5f88a1c32be8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846536834909fb6b3464aae2fb1bd8fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d95a3ac3e72d68d8b99",
+    "url": "/static/css/main.6cc22784.chunk.css"
+  },
+  {
+    "revision": "a7c8441143621dbdcc90",
+    "url": "/static/js/2.2dcdd0f5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2dcdd0f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d95a3ac3e72d68d8b99",
+    "url": "/static/js/main.212450f5.chunk.js"
+  },
+  {
+    "revision": "8e42e4e40bbbee2dca6a",
+    "url": "/static/js/runtime-main.c7bad3dc.js"
+  },
+  {
+    "revision": "419d04c52e42782e92d59397be5dee11",
+    "url": "/static/media/contact.419d04c5.png"
+  },
+  {
+    "revision": "e21a0b675b688ec180d85027a901bfe3",
+    "url": "/static/media/cover.e21a0b67.png"
+  },
+  {
+    "revision": "f4cfe0a367825ba3442d884be6525dc9",
+    "url": "/static/media/draft.f4cfe0a3.png"
+  },
+  {
+    "revision": "1005cce8aa9dfd297433f771ea2164b1",
+    "url": "/static/media/dripping_scold.1005cce8.png"
+  },
+  {
+    "revision": "43b51ab79f423a5e74277cac866af972",
+    "url": "/static/media/plane_brother.43b51ab7.png"
+  },
+  {
+    "revision": "fe7bd4aea8695b717f7b7c622fb9a720",
+    "url": "/static/media/slingshot.fe7bd4ae.png"
+  }
+]);
