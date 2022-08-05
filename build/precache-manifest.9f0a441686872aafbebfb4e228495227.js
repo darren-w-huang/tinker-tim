ec453ac213ecfc6278553ee5ac2375c6fe2eb4e4
darren <dh475@uw.edu>
2022-08-04 23:11:22 -0700

    changed build

---
+++ build/precache-manifest.9f0a441686872aafbebfb4e228495227.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1dd4a143e90ec361041c8a76c2d745a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "701f152010e2b25f4f60",
+    "url": "/static/css/main.194399ac.chunk.css"
+  },
+  {
+    "revision": "40b18df6819b5157d570",
+    "url": "/static/js/2.4f8d9d9d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4f8d9d9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "701f152010e2b25f4f60",
+    "url": "/static/js/main.8e597f95.chunk.js"
+  },
+  {
+    "revision": "8e42e4e40bbbee2dca6a",
+    "url": "/static/js/runtime-main.c7bad3dc.js"
+  },
+  {
+    "revision": "c006efa23576cc75557127cb033cde1c",
+    "url": "/static/media/Tinker_Tim_Plane.c006efa2.pdf"
+  },
+  {
+    "revision": "1184392b02efa91cf257684799cc7dab",
+    "url": "/static/media/comforting_tim.1184392b.png"
+  },
+  {
+    "revision": "419d04c52e42782e92d59397be5dee11",
+    "url": "/static/media/contact.419d04c5.png"
+  },
+  {
+    "revision": "e21a0b675b688ec180d85027a901bfe3",
+    "url": "/static/media/cover.e21a0b67.png"
+  },
+  {
+    "revision": "1005cce8aa9dfd297433f771ea2164b1",
+    "url": "/static/media/dripping_scold.1005cce8.png"
+  },
+  {
+    "revision": "bfba6e1c2103c48ec84cfda9e7c30b4e",
+    "url": "/static/media/family.bfba6e1c.png"
+  },
+  {
+    "revision": "1d903823e39f251abd6fe6b4d6911a38",
+    "url": "/static/media/folding_instructions.1d903823.jpg"
+  },
+  {
+    "revision": "43b51ab79f423a5e74277cac866af972",
+    "url": "/static/media/plane_brother.43b51ab7.png"
+  },
+  {
+    "revision": "a22a772c7c9ddbc108dcbb6ae21147e5",
+    "url": "/static/media/sling.a22a772c.png"
+  },
+  {
+    "revision": "fe7bd4aea8695b717f7b7c622fb9a720",
+    "url": "/static/media/slingshot.fe7bd4ae.png"
+  }
+]);
